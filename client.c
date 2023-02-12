ff76d6c6a4fcd74cc9dd8df209c95a2c96554ca1
habitsxd <maxxcosmos12@gmail.com>
2023-02-11 23:11:06 -0600

    version 2 from assignment complete

--- client.c
+++ client.c
@@ -7,26 +7,30 @@
 
 
 int main(){
+    //data section
     int fd;//server file descriptor
     int fd1;//client file descriptor
     char* client = "/tmp/client"; //client fifo filepath
     char* server = "/tmp/server"; //main fifo file path
-
     char client_name[80]; //buffer for client name
-
-    fgets(client_name,sizeof(client_name),stdin); //read in client name from user
-    fd = open(server,O_WRONLY); //open main fifo to pass client name
-    write(fd,client_name,strlen(client_name) + 1); //write the client name to the fifo
-
-    //close(fd); //close main fifo
-    char* client_full_path = strcat(client,client_name);
-    mkfifo(client_full_path,0666); //make client specific fifo
-    fd1 = open(client_full_path, O_RDONLY);//open client fifo as for reading
-    char* dummy = "dummy";
-    write(fd,dummy,strlen(dummy));
+    //data section
+    
+    //read client name from user input, open server fifo, write client name to server fifo
+    fgets(client_name,sizeof(client_name),stdin);
+    fd = open(server,O_WRONLY); 
+    write(fd,client_name,strlen(client_name) + 1);
+    //make client fifo,open client fifo in read only
+    mkfifo(client,0666); 
+    fd1 = open(client, O_RDONLY);
+    //get system call and send to server through client fifo (to be elaborated on later)
+    char* dummy[80];
+    fgets(dummy, sizeof(dummy),stdin);
+    write(fd,dummy,strlen(dummy) + 1);
+    //read back syscall info to print to the client
     char data_buffer[80];
-
     read(fd1,data_buffer,80);
+    printf("%s\n",data_buffer);
+    //close all fifos
     close(fd1);
     close(fd);
 
--- server.c
+++ server.c
@@ -7,24 +7,30 @@
 
 
 int main(){
+    //data section
     char name_buffer[80];//buffer for client name
-    int fd; //file descriptor
-    int fd1;
-
+    int fd; //file descriptor for server fifo
+    int fd1;//file descriptor for client fifo
     char* server = "/tmp/server";//fifo filepath
     char* client = "/tmp/client"; //client specific fifo filepath
+    //data section
 
-    mkfifo(server,0666);//create well-known fifo
-
-    fd = open(server,O_RDONLY);//open fifo in read only
-    read(fd,name_buffer,80);//read client name
-    printf("%s\n",name_buffer); //print client name
-    
-    //close(fd); //close server
-    fd1 = open(strcat(client,name_buffer), O_WRONLY);
+    //create server fifo
+    mkfifo(server,0666);
+    //open server fifo in read only, read client name, print client name on the server
+    fd = open(server,O_RDONLY);
+    read(fd,name_buffer,80);
+    printf("%s\n",name_buffer); 
+    //maintenence - close old server fifo, open new instance
+    close(fd);
+    fd = open(server,O_RDONLY); 
+    //read in client request from server fifo to a data buffer
     char data_buffer[80];
     read(fd,data_buffer,80);
+    //open client server and write to it
+    fd1 = open(client, O_WRONLY);
     write(fd1,data_buffer,strlen(data_buffer) + 1);
+    //close both fifos
     close(fd1);
     close(fd);
 
